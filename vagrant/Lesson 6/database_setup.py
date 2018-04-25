e185f28b2c761f36317f10a246d8786b4168d954
Paul <35120590+paulc1600@users.noreply.github.com>
2018-04-25 13:19:05 -0700

    Initial settings code and table classes

--- vagrant/Lesson 6/database_setup.py
+++ vagrant/Lesson 6/database_setup.py
@@ -1,1 +1,23 @@
-# Just hold place
+import sys
+from sqlalchemy import Column, Foreignkey, Integer, String
+from sqlalchemy.ext.declarative import declarative base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+# Create instance of declarative base
+Base = declarative_base()
+
+# Create Tables for Database
+class Restaurant(Base):
+
+
+class MenuItem(Base):
+
+
+
+
+### End of File Setting ###
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+### Takes classes we'll create and turns them into tables in new database ###
+Base.metadata.create_all(engine)
