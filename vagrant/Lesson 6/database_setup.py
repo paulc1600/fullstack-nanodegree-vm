8a5c35caaa481716f3f8bccd349cb0f1b38d82ea
Paul <35120590+paulc1600@users.noreply.github.com>
2018-04-25 14:46:38 -0700

    Fix import syntax bug

--- vagrant/Lesson 6/database_setup.py
+++ vagrant/Lesson 6/database_setup.py
@@ -1,6 +1,6 @@
 import sys
 from sqlalchemy import Column, Foreignkey, Integer, String
-from sqlalchemy.ext.declarative import declarative base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
