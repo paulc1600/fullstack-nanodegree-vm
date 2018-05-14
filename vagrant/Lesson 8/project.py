c7a5e5d6acd6666c96ed8f1b39d80f5a3961b964
Paul <35120590+paulc1600@users.noreply.github.com>
2018-05-14 16:36:44 -0700

    Move debug code

--- vagrant/Lesson 8/project.py
+++ vagrant/Lesson 8/project.py
@@ -2,8 +2,6 @@ from flask import Flask
 import socket
 
 app = Flask(__name__)
-My_IP = socket.gethostbyname(socket.gethostname())
-print My_IP
 
 @app.route('/')
 @app.route('/hello')
@@ -11,5 +9,7 @@ def HelloWorld():
     return "Hello World"
 
 if __name__ == '__main__':
+    My_IP = socket.gethostbyname(socket.gethostname())
+    print My_IP
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
