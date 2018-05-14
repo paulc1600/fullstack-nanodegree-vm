3be72d74430cd005bf9a3a1bb3443214a58b0942
Paul <35120590+paulc1600@users.noreply.github.com>
2018-05-14 16:42:50 -0700

    2nd attempt read public IP

--- vagrant/Lesson 8/project.py
+++ vagrant/Lesson 8/project.py
@@ -9,7 +9,10 @@ def HelloWorld():
     return "Hello World"
 
 if __name__ == '__main__':
-    My_IP = socket.gethostbyname(socket.gethostname())
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    My_IP = s.getsockname()[0]
+    s.close()
     print My_IP
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
