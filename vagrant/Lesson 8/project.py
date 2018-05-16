344789eca6ddc29e57bb890bf49226d9fb83b16f
Paul P. Carreiro <ctmtest2304@gmail.com>
2018-05-16 16:52:33 -0700

    debug code

--- vagrant/Lesson 8/project.py
+++ vagrant/Lesson 8/project.py
@@ -15,11 +15,15 @@ session = DBSession()
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-    restaurant = session.query(Restaurant).first()
-    #  filter_id = str(Restaurant.id)
-    filter_id = "1"
+    restaurant = session.query(restaurant).first()
+    print restaurant
+    filter_id1 = str(restaurant.id)
+    filter_id2 = str(restaurant[0].id)
+    filter_id = 1
+    print filter_id1
+    print filter_id2
     print filter_id
-    print Restaurant.name
+    print restaurant.name
     items = session.query(MenuItem).filter_by(restaurant_id = filter_id)
     print items
     output = ''
