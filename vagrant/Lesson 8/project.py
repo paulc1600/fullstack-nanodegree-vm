bfdcd09913435d90c2ad41cb73e14a4be423c50c
Paul P. Carreiro <ctmtest2304@gmail.com>
2018-05-17 11:35:52 -0700

    Attemp C

--- vagrant/Lesson 8/project.py
+++ vagrant/Lesson 8/project.py
@@ -18,8 +18,17 @@ def HelloWorld():
     hw_restaurant = session.query(Restaurant).first()
     print hw_restaurant.id                    # Not printing database values, printing local object attribute? 
     print hw_restaurant.name                  # Not printing database values, printing local object attribute?
+	# ------------------------------------------------------- attempt A
     # items = session.query(MenuItem).filter_by(restaurant_id = Restaurant.id) = get ALL menu items???
-    items = session.query(MenuItem).filter_by(restaurant_id = hw_restaurant.id)
+	# ------------------------------------------------------- attempt B
+    # items = session.query(MenuItem).filter_by(restaurant_id = hw_restaurant.id) = breaks 2nd query (this one)
+	# 1
+	# Pizza Palace
+	# SELECT menu_item.name AS menu_item_name, menu_item.id AS menu_item_id, menu_item.course AS menu_item_course, menu_item.description AS menu_item_description, menu_item.price AS menu_item_price, menu_item.restaurant_id AS menu_item_restaurant_id
+	# FROM menu_item
+	# WHERE menu_item.restaurant_id = ?
+    # ------------------------------------------------------- attempt C
+    items = session.query(MenuItem).filter_by(restaurant_id = '1') 
 	
     print items
     output = ''
