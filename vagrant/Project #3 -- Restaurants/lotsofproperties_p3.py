f09c37c1015093ace2f1975222de1b357efbb4ff
Paul P. Carreiro <ctmtest2304@gmail.com>
2018-06-08 16:08:51 -0700

    fixed 2nd syntax error

--- vagrant/Project #3 -- Restaurants/lotsofproperties_p3.py
+++ vagrant/Project #3 -- Restaurants/lotsofproperties_p3.py
@@ -23,7 +23,7 @@ restaurant1 = session.query(Restaurant).filter(qs1).one()
 RestProperty1 = RestProperties(street="1777 Pike Street", city="San Diego", state="CA", zip="92126", phone="619-809-0123", review_rating="3.5", rest_photo_file="FD1_Restaurant.jpg", 
                                open_Mon="9:30 AM", close_Mon="2:00 PM", open_Tue="9:30 AM", close_Tue="2:00 PM", open_Wed="9:30 AM", close_Wed="10:30 PM", 
                                open_Thu="9:30 AM", close_Thu="10:30 PM", open_Fri="9:30 AM", close_Fri="10:30 PM", open_Sat="11:00 AM", close_Sat="10:30 PM", 
-                               open_Sun="11:00 AM", close_Sun="10:30 PM, restaurant=restaurant1)
+                               open_Sun="11:00 AM", close_Sun="10:30 PM", restaurant=restaurant1)
 session.add(RestProperty1)
 session.commit()
 
@@ -33,7 +33,7 @@ restaurant1 = session.query(Restaurant).filter(qs1).one()
 RestProperty2 = RestProperties(street="2254 Perine Street", city="Alexandria", state="VA", zip="22370", phone="703-806-8377", review_rating="4.0", rest_photo_file="FD2_Restaurant.jpg", 
                                open_Mon="8:30 AM", close_Mon="9:00 PM", open_Tue="8:30 AM", close_Tue="9:00 PM", open_Wed="8:30 AM", close_Wed="10:30 PM", 
                                open_Thu="8:30 AM", close_Thu="10:30 PM", open_Fri="8:30 AM", close_Fri="10:30 PM", open_Sat="11:00 AM", close_Sat="10:30 PM", 
-                               open_Sun="11:00 AM", close_Sun="10:30 PM, restaurant=restaurant1)
+                               open_Sun="11:00 AM", close_Sun="10:30 PM", restaurant=restaurant1)
 session.add(RestProperty2)
 session.commit()
 
@@ -43,7 +43,7 @@ restaurant1 = session.query(Restaurant).filter(qs1).one()
 RestProperty1 = RestProperties(street="909 Yorkshire Circle", city="Alexandria", state="MN", zip="56308", phone="252-327-5503", review_rating="3.5", rest_photo_file="FD3_Restaurant.jpg", 
                                open_Mon="11:00 AM", close_Mon="9:00 PM", open_Tue="11:00 AM", close_Tue="9:00 PM", open_Wed="11:00 AM", close_Wed="10:30 PM", 
                                open_Thu="11:00 AM", close_Thu="10:30 PM", open_Fri="11:00 AM", close_Fri="10:30 PM", open_Sat="11:00 AM", close_Sat="10:30 PM", 
-                               open_Sun="11:00 AM", close_Sun="10:30 PM, restaurant=restaurant1)
+                               open_Sun="11:00 AM", close_Sun="10:30 PM", restaurant=restaurant1)
 session.add(RestProperty1)
 session.commit()
 
@@ -52,7 +52,7 @@ restaurant1 = session.query(Restaurant).filter(qs1).one()
 RestProperty2 = RestProperties(street="3110 Doctors Drive", city="Los Angeles", state="CA", zip="90017", phone="310-341-3892", review_rating="4.0", rest_photo_file="FD4_Restaurant.jpg", 
                                open_Mon="12:00 PM", close_Mon="9:00 PM", open_Tue="12:00 PM", close_Tue="9:00 PM", open_Wed="12:00 PM", close_Wed="10:30 PM", 
                                open_Thu="12:00 PM", close_Thu="10:30 PM", open_Fri="12:00 PM", close_Fri="10:30 PM", open_Sat="12:00 PM", close_Sat="10:30 PM", 
-                               open_Sun="Closed", close_Sun="Closed, restaurant=restaurant1)
+                               open_Sun="Closed", close_Sun="Closed", restaurant=restaurant1)
 session.add(RestProperty2)
 session.commit()
 
@@ -62,7 +62,7 @@ restaurant1 = session.query(Restaurant).filter(qs1).one()
 RestProperty1 = RestProperties(street="3079 Joyce Street", city="Gulf Shores", state="AL", zip="36542", phone="251-968-2181", review_rating="3.0", rest_photo_file="FD4_Restaurant.jpg", 
                                open_Mon="12:00 PM", close_Mon="9:00 PM", open_Tue="12:00 PM", close_Tue="9:00 PM", open_Wed="12:00 PM", close_Wed="10:30 PM", 
                                open_Thu="12:00 PM", close_Thu="10:30 PM", open_Fri="12:00 PM", close_Fri="10:30 PM", open_Sat="12:00 PM", close_Sat="10:30 PM", 
-                               open_Sun="Closed", close_Sun="Closed, restaurant=restaurant1)
+                               open_Sun="Closed", close_Sun="Closed", restaurant=restaurant1)
 session.add(RestProperty1)
 session.commit()
 
@@ -72,7 +72,7 @@ restaurant1 = session.query(Restaurant).filter(qs1).one()
 RestProperty2 = RestProperties(street="3155 Sampson Street", city="Aurora", state="CO", zip="80014", phone="303-568-6185", review_rating="4.0", rest_photo_file="FD5_Restaurant.jpg", 
                                open_Mon="3:00 PM", close_Mon="10:30 PM", open_Tue="3:00 PM", close_Tue="10:30 PM", open_Wed="3:00 PM", close_Wed="10:30 PM", 
                                open_Thu="3:00 PM", close_Thu="10:30 PM", open_Fri="3:00 PM", close_Fri="10:30 PM", open_Sat="12:00 PM", close_Sat="10:30 PM", 
-                               open_Sun="Closed", close_Sun="Closed, restaurant=restaurant1)
+                               open_Sun="Closed", close_Sun="Closed", restaurant=restaurant1)
 session.add(RestProperty2)
 session.commit()
 
@@ -82,7 +82,7 @@ restaurant1 = session.query(Restaurant).filter(qs1).one()
 RestProperty1 = RestProperties(street="3418 Counts Lane", city="West Hartford", state="CT", zip="06105", phone="860-231-3576", review_rating="4.0", rest_photo_file="FD6_Restaurant.jpg", 
                                open_Mon="4:30 PM", close_Mon="10:30 PM", open_Tue="4:30 PM", close_Tue="10:30 PM", open_Wed="4:30 PM", close_Wed="10:30 PM", 
                                open_Thu="4:30 PM", close_Thu="10:30 PM", open_Fri="4:30 PM", close_Fri="10:30 PM", open_Sat="12:00 PM", close_Sat="10:30 PM", 
-                               open_Sun="Closed", close_Sun="Closed, restaurant=restaurant1)
+                               open_Sun="Closed", close_Sun="Closed", restaurant=restaurant1)
 session.add(RestProperty1)
 session.commit()
 
@@ -92,7 +92,7 @@ restaurant1 = session.query(Restaurant).filter(qs1).one()
 RestProperty2 = RestProperties(street="3200 Bailey Drive", city="Iowa City", state="IA", zip="52240", phone="319-337-8002", review_rating="4.0", rest_photo_file="FD7_Restaurant.jpg", 
                                open_Mon="5:30 PM", close_Mon="10:30 PM", open_Tue="5:30 PM", close_Tue="10:30 PM", open_Wed="5:30 PM", close_Wed="10:30 PM", 
                                open_Thu="5:30 PM", close_Thu="10:30 PM", open_Fri="5:30 PM", close_Fri="10:30 PM", open_Sat="12:00 PM", close_Sat="10:30 PM", 
-                               open_Sun="Closed", close_Sun="Closed, restaurant=restaurant1)
+                               open_Sun="Closed", close_Sun="Closed", restaurant=restaurant1)
 session.add(RestProperty2)
 session.commit()
 
@@ -101,7 +101,7 @@ restaurant1 = session.query(Restaurant).filter(qs1).one()
 RestProperty1 = RestProperties(street="3706 Wescam Court", city="Fallon", state="NV", zip="89406", phone="775-867-4121", review_rating="4.0", rest_photo_file="FD8_Restaurant.jpg", 
                                open_Mon="5:30 PM", close_Mon="10:30 PM", open_Tue="5:30 PM", close_Tue="10:30 PM", open_Wed="5:30 PM", close_Wed="10:30 PM", 
                                open_Thu="5:30 PM", close_Thu="10:30 PM", open_Fri="5:30 PM", close_Fri="10:30 PM", open_Sat="12:00 PM", close_Sat="10:30 PM", 
-                               open_Sun="Closed", close_Sun="Closed, restaurant=restaurant1)
+                               open_Sun="Closed", close_Sun="Closed", restaurant=restaurant1)
 session.add(RestProperty1)
 session.commit()
 
