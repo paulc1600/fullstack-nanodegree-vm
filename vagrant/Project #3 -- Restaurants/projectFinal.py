d96160c13d92f4b45b9afbce651e4c59d849459e
paulc1600 <ctmtest2304@gmail.com>
2018-06-08 20:28:33 -0700

    rid of dummy data

--- vagrant/Project #3 -- Restaurants/projectFinal.py
+++ vagrant/Project #3 -- Restaurants/projectFinal.py
@@ -8,8 +8,6 @@ from database_setup import Base, Restaurant, MenuItem
 # ------------------------------------------------------------#
 restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
 
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
 #Fake Menu Items
 items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
 item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
@@ -34,7 +32,8 @@ session = DBSession()
 @app.route('/restaurant/')
 @app.route('/restaurant/list/')
 def showRestaurants():
-	return render_template('Main_List.html', restaurants=restaurants)
+    restaurant_list = session.query(Restaurant).all()
+    return render_template('Main_List.html', restaurants=restaurant_list)
 
 # ------------------------------------------------------------#
 #  Create A New Restaurant Record                             #
