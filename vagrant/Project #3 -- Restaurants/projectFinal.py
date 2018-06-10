7f17af09133947ce61b2f5f3260eeb7f56057804
paulc1600 <ctmtest2304@gmail.com>
2018-06-09 21:10:34 -0700

    same as before 2

--- vagrant/Project #3 -- Restaurants/projectFinal.py
+++ vagrant/Project #3 -- Restaurants/projectFinal.py
@@ -54,8 +54,8 @@ def editRestaurant(restaurant_id):
                         'open_Thu':'9:30 AM','close_Thu':'10:30 PM','open_Fri':'9:30 AM','close_Fri':'10:30 PM','open_Sat':'11:00 AM','close_Sat':'10:30 PM',
                         'open_Sun':'11:00 AM','close_Sun':'10:30 PM'}
     photo_file = "'" + str(properties_2edit['rest_photo_file']) + "'"
-    photo_path = url_for('static', filename = photo_file)
-    print(photo_path)
+    photo_path2edit = url_for('static', filename = photo_file)
+    print(photo_path2edit)
 	
     if request.method == 'POST':
         if request.form['name']:
@@ -108,8 +108,8 @@ def deleteRestaurant(restaurant_id):
                         'open_Thu':'9:30 AM','close_Thu':'10:30 PM','open_Fri':'9:30 AM','close_Fri':'10:30 PM','open_Sat':'11:00 AM','close_Sat':'10:30 PM',
                         'open_Sun':'11:00 AM','close_Sun':'10:30 PM'}
     photo_file = "'" + str(properties_2del['rest_photo_file']) + "'"
-    photo_path = url_for('static', filename = photo_file)
-    print(photo_path)
+    photo_path2del = url_for('static', filename = photo_file)
+    print(photo_path2del)
 	
     if request.method == 'POST':
         if request.form['name']:
