bf5db8d61b4f1a453c71dfed4fb902e72845ec01
Paul P. Carreiro <ctmtest2304@gmail.com>
2018-06-12 14:07:21 -0700

    debug tables

--- vagrant/Project #3 -- Restaurants/projectFinal.py
+++ vagrant/Project #3 -- Restaurants/projectFinal.py
@@ -30,14 +30,14 @@ session = DBSession()
 @app.route('/restaurant/list/')
 def showRestaurants():
     restaurant_list = session.query(Restaurant).all()
-    restaurant_dict = dict((row[0], list(row[1:])) for row in restaurant_list)
-    nbr_rest = len(set(open(restaurant_dict).read().split()))
+    # restaurant_dict = dict((row[0], list(row[1:])) for row in restaurant_list)
+    # nbr_rest = len(set(open(restaurant_dict).read().split()))
     
     properties_list = session.query(RestProperties).all()
-    properties_dict = dict((row[0], list(row[1:])) for row in properties_list)
+    # properties_dict = dict((row[0], list(row[1:])) for row in properties_list)
     
-    print(str(restaurant_dict))
-    print(str(properties_dict))
+    print(str(restaurant_list))
+    print(str(properties_list))
 	# for rec in range(1, nbr_rest+1):
 	
 	# for restaurant_id, value in restaurant_dict:
