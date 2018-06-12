3734f56fe819de69d100227238493cee2a63d032
Paul P. Carreiro <ctmtest2304@gmail.com>
2018-06-11 17:01:11 -0700

    navigation syntax problem

--- vagrant/Project #3 -- Restaurants/projectFinal.py
+++ vagrant/Project #3 -- Restaurants/projectFinal.py
@@ -30,8 +30,7 @@ session = DBSession()
 @app.route('/restaurant/list/')
 def showRestaurants():
     restaurant_list = session.query(Restaurant).all()
-    properties_list = session.query(RestProperties).all()
-    return render_template('Main_List.html', restaurants=restaurant_list, properties=properties_list)
+    return render_template('Main_List.html', restaurants=restaurant_list)
 
 # ------------------------------------------------------------#
 #  Create A New Restaurant Record                             #
