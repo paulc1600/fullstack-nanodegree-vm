6c4abea023278f7ae76ff9d565376fe0b169eb79
Paul P. Carreiro <ctmtest2304@gmail.com>
2018-06-25 11:05:06 -0700

    add nice comments to code

--- vagrant/Lesson 11/OAuth2.0-master/project.py
+++ vagrant/Lesson 11/OAuth2.0-master/project.py
@@ -125,18 +125,23 @@ def gdisconnect():
     access_token = login_session.get('access_token')
     if access_token is None:
         print 'Access Token is None'
+		# If token does not exist, there is no one to disconnect
         response = make_response(json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     print 'In gdisconnect access token is %s', access_token
     print 'User name is: '
     print login_session['username']
+	# Send current access token to Google so they can revoke it 
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
     h = httplib2.Http()
+	# store Google's response in object = result
     result = h.request(url, 'GET')[0]
     print 'result is '
     print result
+	# A response of 200 means user was successfully disconnected
     if result['status'] == '200':
+	    # Delete user information from the login_session object
         del login_session['access_token']
         del login_session['gplus_id']
         del login_session['username']
