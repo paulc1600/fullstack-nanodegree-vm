4e1dee75fc395d0e5247f29b7ead2fc5b0a2fa47
paulc1600 <ctmtest2304@gmail.com>
2018-06-27 19:12:20 -0700

    put away changes

--- vagrant/Lesson 11/OAuth2.0-master/project.py
+++ vagrant/Lesson 11/OAuth2.0-master/project.py
@@ -5,14 +5,12 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
-
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
