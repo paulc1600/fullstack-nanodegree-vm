e2c9562137d6edf388a65cf4b9630cb854288249
paulc1600 <ctmtest2304@gmail.com>
2018-06-27 20:18:00 -0700

    add user table
    for local permission system

--- vagrant/Lesson 11/OAuth2.0-master/database_setup.py
+++ vagrant/Lesson 11/OAuth2.0-master/database_setup.py
@@ -5,12 +5,22 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    email = Column(String(120), nullable=False)
+    picture = Column(String(250))
+	
 class Restaurant(Base):
     __tablename__ = 'restaurant'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
+	
     @property
     def serialize(self):
        """Return object data in easily serializeable format"""
@@ -22,15 +32,15 @@ class Restaurant(Base):
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
-
     name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    user_id = Column(Integer,ForeignKey('user.id'))
     restaurant = relationship(Restaurant)
-
+    user = relationship(User)
 
     @property
     def serialize(self):
