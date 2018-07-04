a55fb75a0c680402e898c43b5c139c43183fb183
paulc1600 <ctmtest2304@gmail.com>
2018-07-04 15:00:28 -0700

    fix syntax bug

--- vagrant/Lesson 11/OAuth2.0-master/project.py
+++ vagrant/Lesson 11/OAuth2.0-master/project.py
@@ -186,10 +186,10 @@ def fbconnect():
 	# Exchange client token for long-lived server-side token
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
     app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' %
-	    (app_id,app_secret,access_token)
+    
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id,app_secret,access_token)
     h = httplib2.Http()
-	result = h.request(url, 'GET')[1]
+    result = h.request(url, 'GET')[1]
 	
 	#Use token to get user info from API
     userinfo_url = "https://graph.facebook.com/v2.8/me"
