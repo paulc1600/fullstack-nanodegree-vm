9a718465e03cd6def4bb47c3bffa4a3fa5e68d27
Paul P. Carreiro <ctmtest2304@gmail.com>
2018-07-11 14:54:51 -0700

    debug partial

--- vagrant/Lesson 15/geocode.py
+++ vagrant/Lesson 15/geocode.py
@@ -20,8 +20,8 @@ def getGeocodeLocation(inputString):
     latitude = 0
     longitude = 0
 	# print lat and longitude values wanted from JSON data
-    # latitude = result['results'][0]['geometry']['location']['lat']
-    # longitude = result['results'][0]['geometry']['location']['lng']
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
     print "Latitude: %s \n \n" % latitude
     print "Longitude: %s \n \n" % longitude
     return (latitude, longitude)
